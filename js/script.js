6135f06a647dd9649834127a1c0a55f28bf5ea5f
Marcin Tonder <ginofelino000@gmail.com>
2022-07-27 14:25:31 +0200

    clicked link active

--- js/script.js
+++ js/script.js
@@ -5,33 +5,32 @@
 
 
 const titleClickHandler = function(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
-  console.log('event:', event);
+  
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
-for(let activeLink of activeLinks){
+  for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
-}
-  /* add class 'active' to the clicked link */
-   const activeArticles = document.querySelectorAll('.post active');
+  }
+  /* [DONE] add class 'active' to the clicked link */
+  clickedElement.classList.add('active');
+  console.log('clickedElement:' , clickedElement);
 
-for(let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');
-}
-  /* remove class 'active' from all articles */
+  /* [DONE] remove class 'active' from all articles */
 
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
   /* add class 'active' to the correct article */
-}
+  }
 
-const links = document.querySelectorAll('.titles a');
+  const links = document.querySelectorAll('.titles a');
 
-for(let link of links){
+  for(let link of links){
   link.addEventListener('click', titleClickHandler);
-}
+  }
 
